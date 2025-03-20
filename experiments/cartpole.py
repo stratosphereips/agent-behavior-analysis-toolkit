33864c396c429a0257d0344b2fd026eab886e7bc
Ondrej Lukas <ondrej.lukas95@gmail.com>
2025-03-20 13:47:35 +0100

    Add REINFORCE

--- experiments/cartpole.py
+++ experiments/cartpole.py
@@ -1,9 +1,11 @@
 
 from environments.gym_discretization_wrapper import CustomEnv
 from agents.dqn import DQN
+from agents.reinforce import REINFORCE
 import gymnasium as gym
 import argparse
 import numpy as np
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -11,10 +13,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # Fix random seed
     np.random.seed(args.seed)
-    env = CustomEnv(gym.make("CartPole-v1"), seed=args.seed)
+    env = gym.make("CartPole-v1")
+    # env = ClipObservation(env)
+    # env = RescaleObservation(env, min_obs=-1, max_obs=1)
+    env = CustomEnv(env, seed=args.seed)
 
-    agent = DQN(env.observation_space.shape[0], env.action_space.n)
-    agent.train_policy(env, num_episodes=5000, evaluate_each=500, evaluate_for=500)
+    #agent = DQN(env.observation_space.shape[0], env.action_space.n)
+    agent = REINFORCE(env.observation_space.shape[0], env.action_space.n)
+    agent.train_policy(env, num_episodes=1000, evaluate_each=100, evaluate_for=200)
     # Final evaluation
     returns, _ = agent.evaluate_policy(env, num_episodes=2000)
     print(f"Final evaluation:{np.mean(returns):.2f}+-{np.std(returns):.2f}")
