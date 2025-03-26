61a9454ba767f2af7889304700ba1c68cd8db66e
Ondrej Lukas <ondrej.lukas95@gmail.com>
2025-03-26 21:41:37 +0100

    wandb support

--- experiments/cartpole_discrete.py
+++ experiments/cartpole_discrete.py
@@ -6,15 +6,32 @@ from agents.dqn import DQN
 import gymnasium as gym
 import argparse
 import numpy as np
+import wandb
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--seed", default=None, type=int, help="Random seed.")
+    parser.add_argument("--seed", default=4242, type=int, help="Random seed.")
+    parser.add_argument("--episodes", default=5000, type=int, help="Number of training episodes")
+    parser.add_argument("--evaluate_each", default=500, type=int, help="Periodic evluation frequency")
+    parser.add_argument("--evaluate_for", default=500, type=int, help="Periodic evluation length")
     args = parser.parse_args()
     # Fix random seed
     np.random.seed(args.seed)
-    #discretized_env = CustomEnv(DiscreteCartPoleWrapper(gym.make("CartPole-v1"), bins=8), seed=args.seed)
-    discretized_env = CustomEnv(gym.make("CartPole-v1"), seed=args.seed)
+    experiment_config = {
+        "env": "CartPole-v1-discrete",
+        "model": "SARSA",
+    }
+    experiment_config.update(vars(args))
+    print(experiment_config)
+    wandb_run = wandb.init(
+        # Set the wandb entity where your project will be logged (generally your team name).
+        entity="ondrej-lukas-czech-technical-university-in-prague",
+        # Set the wandb project where this run will be logged.
+        project="agent-trajectory-analysis",
+        # Track hyperparameters and run metadata.
+        config=experiment_config,
+    ) 
+    discretized_env = CustomEnv(DiscreteCartPoleWrapper(gym.make("CartPole-v1"), bins=8), seed=args.seed)
     
     # agent = Qlearning(
     #     discretized_env.observation_space.n,
@@ -23,25 +40,26 @@ if __name__ == "__main__":
     #     gamma=0.95,
     #     epsilon=1,
     #     epsilon_min=0.01,
-    #     epsilon_decay=0.0001
+    #     epsilon_decay=0.0001,
+    #     wandb_run = wandb_run
     #     )
 
-    # agent = Sarsa(
-    #     discretized_env.observation_space.n,
-    #     discretized_env.action_space.n,
-    #     alpha=0.1,
-    #     gamma=0.95,
-    #     epsilon=1,
-    #     epsilon_min=0.05,
-    #     epsilon_decay=0.0005
-    # )
-    print(discretized_env.action_space.shape)
-    agent = DQN(discretized_env.observation_space.shape[0], discretized_env.action_space.n)
-    
+    agent = Sarsa(
+        discretized_env.observation_space.n,
+        discretized_env.action_space.n,
+        alpha=0.1,
+        gamma=0.95,
+        epsilon=1,
+        epsilon_min=0.05,
+        epsilon_decay=0.0005,
+        wandb_run = wandb_run
+    )
+    print(discretized_env.action_space.shape)  
 
 
-    agent.train_policy(discretized_env, num_episodes=5000, evaluate_each=500, evaluate_for=500)
+    agent.train_policy(discretized_env, num_episodes=args.episodes, evaluate_each=args.evaluate_each, evaluate_for=args.evaluate_for)
     # Final evaluation
     returns, _ = agent.evaluate_policy(discretized_env, num_episodes=2000)
     print(f"Final evaluation:{np.mean(returns):.2f}+-{np.std(returns):.2f}")
+    wandb_run.finish()
     
