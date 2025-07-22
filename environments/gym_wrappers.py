7c04f069cf2e931d4414713a1a1c2a4ba5a050fd
Ondrej Lukas <ondrej.lukas95@gmail.com>
2025-07-22 16:21:17 +0200

    correctly store the trajectory

--- environments/gym_wrappers.py
+++ environments/gym_wrappers.py
@@ -1,4 +1,4 @@
-from trajectory_graph import Transition
+from trajectory import Trajectory, Transition
 import gymnasium as gym
 import numpy as np
 
@@ -25,15 +25,18 @@ class TrajectoryRecorderWrapper(gym.Wrapper):
         super().__init__(env)
         self.record_trajectory = False
         self.trajectory_log = []         # List of full episode transitions
-        self.current_trajectory = []     # Current episode
+        self.current_trajectory = None
         self._last_real_obs = None
 
     def start_recording(self):
         self.record_trajectory = True
-        self.current_trajectory = []
+        self.current_trajectory = Trajectory()
 
     def stop_recording(self):
         self.record_trajectory = False
+    
+    def clear_trajectory_log(self)->None:
+        self.trajectory_log = []
 
     def reset(self, **kwargs):
         result = self.env.reset(**kwargs)
@@ -45,7 +48,7 @@ class TrajectoryRecorderWrapper(gym.Wrapper):
             info = {}
 
         if self.record_trajectory:
-            self.current_trajectory = []
+            self.current_trajectory = Trajectory()
 
         self._last_real_obs = obs.copy() if hasattr(obs, "copy") else np.array(obs)
         return result
@@ -60,17 +63,15 @@ class TrajectoryRecorderWrapper(gym.Wrapper):
             else:
                 s = self._last_real_obs.copy()
                 s_next = obs.copy()
-            self.current_trajectory.append(
-                Transition(
-                    state=s,
-                    action=action,
-                    reward=reward,
-                    next_state=s_next
-                )
+            self.current_trajectory.add_transition(
+                state=s,
+                action=action,
+                reward=reward,
+                next_state=s_next
             )
         self._last_real_obs = obs.copy()
 
         if self.record_trajectory and (done or truncated):
-            self.trajectory_log.append(self.current_trajectory.copy())
+            self.trajectory_log.append(self.current_trajectory)
 
         return obs, reward, done, truncated, info
