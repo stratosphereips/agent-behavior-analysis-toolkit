dd66ef76af52266414ab22b800bbbe9bde58f965
Ondrej Lukas <ondrej.lukas95@gmail.com>
2025-08-19 11:37:08 +0200

    move visualization outside of base agent

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -1,17 +1,19 @@
 import numpy as np
 from trajectory_graph import TrajectoryGraph, Transition, plot_tg_mdp
 from trajectory import EmpiricalPolicy
-from utils.trajectory_utils import get_motifs, empirical_policy_statistics, find_trajectory_segments, cluster_segments
+from utils.trajectory_utils import get_motifs, empirical_policy_statistics, find_trajectory_segments, cluster_segments, store_trajectories_to_json
 from utils.plotting_utils import plot_trajectory_segments, plot_segment_cluster_features, plot_action_per_step_distribution, plot_trajectory_network_colored_nodes_by_cluster
+from utils.trajectory_utils import NumpyEncoder
 import wandb
 import matplotlib.pyplot as plt
 from PIL import Image
 import io
+import os
 
 
 class Agent:
     def __init__(self, **kwargs):
-        self.params = kwargs  # Store parameters in a dictionary       
+        self.params = kwargs  # Store parameters in a dictionary)
         self._initialize_agent()
         self.wandb_run = kwargs.get("wandb_run", None)
         self._chechpoint_id = 0
@@ -47,59 +49,85 @@ class Agent:
             returns.append(ret)
         if self.wandb_run is not None:
             if not final_evaluation:
-                trajectories = env.trajectory_log              
-                empirical_policy = EmpiricalPolicy(trajectories)
-                log_data = {"static_graph_metrics":empirical_policy_statistics(empirical_policy, is_win_fn=env.is_win_fn)}
-                log_data["Cluster Feature Summary"] = None
-                log_data["Segment Surprise Plot"] = None
-                log_data['segmentation_metrics'] = {     # Initialize with default/zero values
-                    "segments": 0,
-                    "unique_segments": 0,
-                    "clusters": 0,
-                    "mean_segment_in_cluster": 0.0,
-                    "mean_unique_segment_in_cluster": 0.0,
-                    "unique_trajectories":len(set(trajectories))
-                }
-                if self._previous_policy:
-                    segments = []
-                    for t in trajectories:
-                        new_segments = find_trajectory_segments(trajectory=t, policy=empirical_policy, previous_policy=self._previous_policy)
-                        segments += new_segments
-                    if len(segments) > 0:
-                        log_data['segmentation_metrics']["segments"] = len(segments)
-                        log_data['segmentation_metrics']["unique_segments"] = len(set([tuple(s["features"].values()) for s in segments]))
-                        clustering  = cluster_segments(segments)
-                        segments_per_cluster = []
-                        unique_segments_per_cluster = []
-                        unique_segment_clusters = {}
-                        for cluster_id, segments in clustering.items():
+                trajectories = env.trajectory_log
+                metadata = {k:v for k,v in self.params.items() if k not in ["wandb_run", "discretized_env.observation_space.n", "discretized_env.action_space.n"]}
+                print(metadata)
+                foldername = ""
+                try:
+                    foldername += metadata["experiment_config"]["env"]
+                except KeyError:
+                    pass
+                try:
+                    foldername = foldername + f"_{metadata['experiment_config']['model']}"
+                except KeyError:
+                    pass
 
-                            segments_per_cluster.append(len(segments))
-                            s = set()
-                            unique_segment_clusters[cluster_id] = []
-                            for segment in segments:
-                                if tuple(segment["features"].values()) not in s:
-                                    unique_segment_clusters[cluster_id].append(segment)
-                                    s.add(tuple(segment["features"].values()))
-                            unique_segments_per_cluster.append(len(s))
-                        cluster_summary_fig = plot_segment_cluster_features(clustering)
-                        log_data["Cluster Feature Summary"] = wandb.Image(cluster_summary_fig, caption="Feature Summary per cluster")
-                        plt.close(cluster_summary_fig)  # clean up
-                        segments_plot = plot_trajectory_segments(trajectories, empirical_policy, self._previous_policy,f"Surprise_plot_cp_{self._chechpoint_id}")
-                        log_data["Segment Surprise Plot"]  = wandb.Image(segments_plot, caption="Surprise per step in all trajectories")
-                        plt.close(segments_plot) #cleanup
-                        log_data['segmentation_metrics']["clusters"] = len(clustering)
-                        log_data['segmentation_metrics']["mean_segment_in_cluster"] = np.mean(segments_per_cluster)
-                        log_data['segmentation_metrics']["mean_unique_segment_in_cluster"] = np.mean(unique_segments_per_cluster)
-                        action_distribution_plot = plot_action_per_step_distribution(trajectories, env.action_space.n, normalize=True)
-                        log_data["Action Distribution Plot"] = wandb.Image(action_distribution_plot, caption="Action Distribution per Time Step")
-                        plt.close(action_distribution_plot)  # cleanup
-                        trajectory_plot = plot_trajectory_network_colored_nodes_by_cluster(trajectories[0], unique_segment_clusters)
+                foldername = f"trajectories/{foldername}"
+                if not os.path.exists(foldername):
+                    os.makedirs(foldername)
+                # Create nested_folder based on parameters
+                try:
+                    nested_foldername = "_".join(f"{k}={metadata["experiment_config"][k]}" for k in sorted(metadata["experiment_config"].keys()) if k not in ["env", "model"])
+                except KeyError:
+                    nested_foldername = ""
+                
+                foldername = f"{foldername}/{nested_foldername}"
+                if not os.path.exists(foldername):
+                    os.makedirs(foldername)
+                filename = f"cp_{self._chechpoint_id:02d}.json"
+                store_trajectories_to_json(trajectories, f"{foldername}/{filename}", metadata=metadata, encoder=NumpyEncoder)
+                # empirical_policy = EmpiricalPolicy(trajectories)
+                # log_data = {"static_graph_metrics":empirical_policy_statistics(empirical_policy, is_win_fn=env.is_win_fn)}
+                # log_data["Cluster Feature Summary"] = None
+                # log_data["Segment Surprise Plot"] = None
+                # log_data['segmentation_metrics'] = {     # Initialize with default/zero values
+                #     "segments": 0,
+                #     "unique_segments": 0,
+                #     "clusters": 0,
+                #     "mean_segment_in_cluster": 0.0,
+                #     "mean_unique_segment_in_cluster": 0.0,
+                #     "unique_trajectories":len(set(trajectories))
+                # }
+                # if self._previous_policy:
+                #     segments = []
+                #     for t in trajectories:
+                #         new_segments = find_trajectory_segments(trajectory=t, policy=empirical_policy, previous_policy=self._previous_policy)
+                #         segments += new_segments
+                #     if len(segments) > 0:
+                #         log_data['segmentation_metrics']["segments"] = len(segments)
+                #         log_data['segmentation_metrics']["unique_segments"] = len(set([tuple(s["features"].values()) for s in segments]))
+                #         clustering  = cluster_segments(segments)
+                #         segments_per_cluster = []
+                #         unique_segments_per_cluster = []
+                #         unique_segment_clusters = {}
+                #         for cluster_id, segments in clustering.items():
 
-                        log_data["Trajectory Network Plot"] = wandb.Image(Image.open(io.BytesIO(trajectory_plot)), caption="Trajectory Network Colored by Cluster") # cleanup
-                        print(log_data)
-                self.wandb_run.log(log_data,step=self._chechpoint_id)
-                self._previous_policy = empirical_policy
+                #             segments_per_cluster.append(len(segments))
+                #             s = set()
+                #             unique_segment_clusters[cluster_id] = []
+                #             for segment in segments:
+                #                 if tuple(segment["features"].values()) not in s:
+                #                     unique_segment_clusters[cluster_id].append(segment)
+                #                     s.add(tuple(segment["features"].values()))
+                #             unique_segments_per_cluster.append(len(s))
+                #         cluster_summary_fig = plot_segment_cluster_features(clustering)
+                #         log_data["Cluster Feature Summary"] = wandb.Image(cluster_summary_fig, caption="Feature Summary per cluster")
+                #         plt.close(cluster_summary_fig)  # clean up
+                #         segments_plot = plot_trajectory_segments(trajectories, empirical_policy, self._previous_policy,f"Surprise_plot_cp_{self._chechpoint_id}")
+                #         log_data["Segment Surprise Plot"]  = wandb.Image(segments_plot, caption="Surprise per step in all trajectories")
+                #         plt.close(segments_plot) #cleanup
+                #         log_data['segmentation_metrics']["clusters"] = len(clustering)
+                #         log_data['segmentation_metrics']["mean_segment_in_cluster"] = np.mean(segments_per_cluster)
+                #         log_data['segmentation_metrics']["mean_unique_segment_in_cluster"] = np.mean(unique_segments_per_cluster)
+                #         action_distribution_plot = plot_action_per_step_distribution(trajectories, env.action_space.n, normalize=True)
+                #         log_data["Action Distribution Plot"] = wandb.Image(action_distribution_plot, caption="Action Distribution per Time Step")
+                #         plt.close(action_distribution_plot)  # cleanup
+                #         trajectory_plot = plot_trajectory_network_colored_nodes_by_cluster(trajectories[0], unique_segment_clusters)
+
+                #         log_data["Trajectory Network Plot"] = wandb.Image(Image.open(io.BytesIO(trajectory_plot)), caption="Trajectory Network Colored by Cluster") # cleanup
+                #         print(log_data)
+                # self.wandb_run.log(log_data,step=self._chechpoint_id)
+                # self._previous_policy = empirical_policy
                 self._chechpoint_id += 1
         # stop the trajectory recording
         env.stop_recording()
