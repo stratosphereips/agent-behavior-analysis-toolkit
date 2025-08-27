449d314dc9838e76ba68a9f0bc73ae4c4eec06d3
Ondrej Lukas <ondrej.lukas95@gmail.com>
2025-08-27 08:28:24 +0200

    Add support for jsonl

--- experiments/trajectory_replay.py
+++ experiments/trajectory_replay.py
@@ -3,7 +3,7 @@ from utils.trajectory_utils import empirical_policy_statistics
 from utils.trajectory_utils import find_trajectory_segments, cluster_segments
 from utils.plotting_utils   import plot_segment_cluster_features, plot_trajectory_network_colored_nodes_by_cluster
 from utils.plotting_utils   import plot_trajectory_segments, plot_action_per_step_distribution, plot_trajectory_heatmap
-from utils.plotting_utils   import plot_trajectory_heatmap
+from utils.plotting_utils   import plot_trajectory_heatmap, visualize_clusters
 from utils.trajectory_utils import get_trajectory_action_change
 from utils.trajectory_utils import get_clusters_per_step
 from utils.trajectory_utils import compute_trajectory_surprises
@@ -26,7 +26,7 @@ class TrajectoryReplay:
 
     def __init__(self, trajectory_dir, **kwargs):
         self.trajectories = []
-        self.json_files = sorted([os.path.join(trajectory_dir, f) for f in os.listdir(trajectory_dir) if f.endswith(".json")])
+        self.json_files = sorted([os.path.join(trajectory_dir, f) for f in os.listdir(trajectory_dir) if f.endswith(".json") or f.endswith(".jsonl")])
         print(f"Found {len(self.json_files)} JSON files in {trajectory_dir}")
         self._previous_policy = None
         wandb_project = kwargs.get("wandb_project", None)
@@ -38,12 +38,12 @@ class TrajectoryReplay:
 
     def process_trajectories(self):
         for checkpoint_id, json_file in enumerate(self.json_files):
-            trajectories, trajectory_metadata = load_trajectories_from_json(json_file, load_metadata=True, max_trajectories=200)
+            trajectories, trajectory_metadata = load_trajectories_from_json(json_file, load_metadata=True, max_trajectories=4)
             print(f"Loaded {len(trajectories)} trajectories from {json_file}: - checkpoint {checkpoint_id}")
             self.trajectories.extend(trajectories)
             empirical_policy = EmpiricalPolicy(trajectories)
             print(f"Total_actions: {empirical_policy.num_actions}")
-            print(trajectory_metadata)
+            # print(trajectory_metadata)
 
 
             log_data = {"static_graph_metrics":empirical_policy_statistics(empirical_policy)}
@@ -59,8 +59,8 @@ class TrajectoryReplay:
             }
             if self._previous_policy:
                 segments = []
-                for t in trajectories:
-                    new_segments = find_trajectory_segments(trajectory=t, policy=empirical_policy, previous_policy=self._previous_policy)
+                for traj_idx, t in enumerate(trajectories):
+                    new_segments = find_trajectory_segments(trajectory=t, policy=empirical_policy, previous_policy=self._previous_policy, trajectory_id=f"{checkpoint_id}_{traj_idx}")
                     segments += new_segments
                 if len(segments) > 0:
                     log_data['segmentation_metrics']["segments"] = len(segments)
@@ -89,27 +89,39 @@ class TrajectoryReplay:
                         log_data['segmentation_metrics']["clusters"] = len(clustering)
                         log_data['segmentation_metrics']["mean_segment_in_cluster"] = np.mean(segments_per_cluster)
                         log_data['segmentation_metrics']["mean_unique_segment_in_cluster"] = np.mean(unique_segments_per_cluster)
-                        action_distribution_plot = plot_action_per_step_distribution(trajectories, 2, normalize=True)
+                        num_actions = 6 #empirical_policy.num_actions
+                        action_distribution_plot = plot_action_per_step_distribution(trajectories, num_actions, normalize=True)
                         log_data["Action Distribution Plot"] = wandb.Image(action_distribution_plot, caption="Action Distribution per Time Step")
                         plt.close(action_distribution_plot)  # cleanup
                         
                         unique_trajectories = set(trajectories)
                         max_len = max(len(t) for t in unique_trajectories)
-                        surprises = np.zeros((len(unique_trajectories), max_len))
-                        action_change = np.zeros((len(unique_trajectories), max_len))
-                        cluster_per_step = np.zeros((len(unique_trajectories), max_len))
-                        for i, trajectory in enumerate(unique_trajectories):
-                            surprises[i, :len(trajectory)] = compute_trajectory_surprises(trajectory, empirical_policy, self._previous_policy)
-                            action_change[i, :len(trajectory)] = get_trajectory_action_change(trajectory, empirical_policy, self._previous_policy)
-                            cluster_per_step[i, :len(trajectory)] = get_clusters_per_step(trajectory, clustering)
-                        trajectory_heatmap_plot = plot_trajectory_heatmap(surprises, action_change, cluster_per_step)[0]
-                        # trajectory_plot = plot_trajectory_network_colored_nodes_by_cluster(trajectories[0], unique_segment_clusters)
+                        # surprises = np.zeros((len(unique_trajectories), max_len))
+                        # action_change = np.zeros((len(unique_trajectories), max_len))
+                        # cluster_per_step = np.zeros((len(unique_trajectories), max_len))
+                        # for i, trajectory in enumerate(unique_trajectories):
+                        #     surprises[i, :len(trajectory)] = compute_trajectory_surprises(trajectory, empirical_policy, self._previous_policy)
+                        #     action_change[i, :len(trajectory)] = get_trajectory_action_change(trajectory, empirical_policy, self._previous_policy)
+                        #     cluster_per_step[i, :len(trajectory)] = get_clusters_per_step(trajectory, clustering)
+                        # trajectory_heatmap_plot = plot_trajectory_heatmap(surprises, action_change, cluster_per_step)[0]
+                        # # trajectory_plot = plot_trajectory_network_colored_nodes_by_cluster(trajectories[0], unique_segment_clusters)
 
-                        log_data["Trajectory Combined Heatmap"] = wandb.Image(trajectory_heatmap_plot, caption="Trajectory Combined Heatmap")
-                        plt.close(trajectory_heatmap_plot)
+                        # log_data["Trajectory Combined Heatmap"] = wandb.Image(trajectory_heatmap_plot, caption="Trajectory Combined Heatmap")
+                        # plt.close(trajectory_heatmap_plot)
                         #trajectory_plot = plot_trajectory_network_colored_nodes_by_cluster(trajectories[0], unique_segment_clusters)
 
                         #log_data["Trajectory Network Plot"] = wandb.Image(Image.open(io.BytesIO(trajectory_plot)), caption="Trajectory Network Colored by Cluster") # cleanup
+                        for cluster_id,segments in clustering.items():
+                            traj_ids = set()
+                            for seg in segments:
+                                traj_ids.add(seg["trajectory_id"])
+                            print(f"\tCluster {cluster_id} contains segments from {traj_ids} trajectories.")
+                        cluster_visualization = visualize_clusters(
+                            clustering,
+                            max_len,
+                        )
+                        log_data["Cluster Visualization"] = wandb.Image(cluster_visualization, caption="Cluster Visualization")
+                        plt.close(cluster_visualization)
                     print(log_data)
             if self._wandb_run:
                 wandb.config.update(trajectory_metadata)
@@ -119,5 +131,6 @@ class TrajectoryReplay:
 if __name__ == "__main__":
     trajectory_replay = TrajectoryReplay(sys.argv[1],
     wandb_project="agent-trajectory-analysis",
-    wandb_entity="ondrej-lukas-czech-technical-university-in-prague")
+    wandb_entity="ondrej-lukas-czech-technical-university-in-prague"
+    )
     trajectory_replay.process_trajectories()
