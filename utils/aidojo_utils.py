3d0cec2b1e2325033754311af66c36ea8d563034
Ondrej Lukas <ondrej.lukas95@gmail.com>
2025-11-20 20:18:42 +0100

    Add ActionType import and implement action type decoding from dictionary

--- utils/aidojo_utils.py
+++ utils/aidojo_utils.py
@@ -1,4 +1,4 @@
-from AIDojoCoordinator.game_components import GameState, Action
+from AIDojoCoordinator.game_components import GameState, Action,ActionType
 from AIDojoCoordinator.utils.utils import state_as_ordered_string
 from trajectory import Trajectory
 import json
@@ -14,3 +14,22 @@ def aidojo_rebuild_trajectory(states:Iterable[Dict[str, Any]], actions:Iterable[
     for s, a, r, s_next in zip(states, actions, rewards, states[1:]):
         traj.add_transition(s, a, r, s_next)
     return traj
+
+
+def aidojo_state_str_from_dict(state: Dict[str, Any]) -> str:
+    """
+    Encode a GameState into a string representation.
+    """
+    return state_as_ordered_string(GameState.from_dict(state))
+
+def aidojo_action_from_dict(action: Dict[str, Any]) -> Action:
+    """
+    Decode an Action from its dictionary representation.
+    """
+    return Action.from_dict(action)
+
+def aidojo_action_type_from_dict(action: Dict[str, Any]) -> ActionType:
+    """
+    Decode action from its dictionary representation and keep only the type information (discard action parameters).
+    """
+    return Action.from_dict(action).type
